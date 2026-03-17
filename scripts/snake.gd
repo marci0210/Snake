f9c1f9fa59f37e0389b6bffaf1090deaa95885f8
Mezei Márton <mezeimarton2000@gmail.com>
2026-03-17 18:00:02 +0100

    add the body segments with basic rotations

--- scripts/snake.gd
+++ scripts/snake.gd
@@ -1,7 +1,11 @@
 extends CharacterBody2D
 
 @export var tile_map: TileMapLayer
+@export var body_segment_scene: PackedScene
+
 @onready var design = $Sprite2D_head
+
+var body_segments: Array = []
 
 var grid_size = 40
 var move_delay = 0.5
@@ -12,6 +16,33 @@ var new_target_direction = Vector2.ZERO
 
 var is_moving = true
 var prev_time_stamp = 0
+
+func _ready():
+	$RayCast2D.target_position = current_direction * grid_size
+	$RayCast2D.force_raycast_update()
+	
+	body_segments.append(self)
+	add_initial_segments()
+	add_initial_segments()
+	add_initial_segments()
+	add_initial_segments()
+	add_initial_segments()
+	add_initial_segments()
+
+func add_initial_segments() -> void:
+	var new_segment = body_segment_scene.instantiate()
+	var last_segment = body_segments[-1]
+	if current_direction == Vector2.RIGHT:
+		new_segment.position = last_segment.position + Vector2(-grid_size, 0)
+	elif current_direction == Vector2.LEFT:
+		new_segment.position = last_segment.position + Vector2(grid_size, 0)
+	elif current_direction == Vector2.UP:
+		new_segment.position = last_segment.position + Vector2(0, -grid_size)
+	elif current_direction == Vector2.DOWN:
+		new_segment.position = last_segment.position + Vector2(0, grid_size)
+	new_segment.rotation = last_segment.rotation
+	get_parent().add_child.call_deferred(new_segment)
+	body_segments.append(new_segment)
 
 func _physics_process(_delta):
 	if   Input.is_action_pressed("ui_up") and current_direction != Vector2.DOWN:    
@@ -30,42 +61,50 @@ func _physics_process(_delta):
 	else:
 		if new_target_direction != Vector2.ZERO:
 			current_direction = new_target_direction
+			new_target_direction = Vector2.ZERO
 		
 		# check there is no wall
-		$RayCast2D.target_position = current_direction * grid_size
-		$RayCast2D.force_raycast_update()
+		#$RayCast2D.target_position = current_direction * grid_size
+		#$RayCast2D.force_raycast_update()
+		
+		# first set the new direction of the head
+		#rotation = new_target_direction.angle()
 		
 		# in case of collision, update max score then exit
 		if $RayCast2D.is_colliding():
-			var current_score = get_parent().get_node("CanvasLayer/Score")
-			Global.update_overall_high_score(int(current_score.text))
-			
-			Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
-			get_tree().change_scene_to_file("res://scenes/homepage.tscn")
+			var collider = $RayCast2D.get_collider()
+	
+			if collider is TileMapLayer or $RayCast2D.get_collider().collision_layer == 1: # wall or snake itself
+				var current_score = get_parent().get_node("CanvasLayer/Score")
+				Global.update_overall_high_score(int(current_score.text))
+				
+				Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+				get_tree().change_scene_to_file("res://scenes/homepage.tscn")
+			elif $RayCast2D.get_collider().collision_layer == 2: # object
+				pick_up_object()
 		
 		time_until_next_move = move_delay
 		move_to_grid(current_direction)
 
-func move_to_grid(dir):
-	if dir == Vector2.UP:
-		design.rotation_degrees = 90
-	elif dir == Vector2.DOWN:
-		design.rotation_degrees = -90
-	elif dir == Vector2.LEFT:
-		design.rotation_degrees = 0
-	elif dir == Vector2.RIGHT:
-		design.rotation_degrees = -180
+func move_to_grid(dir):		
+	for i in range(body_segments.size() - 1, 0, -1):
+		body_segments[i].position = body_segments[i-1].position
+		body_segments[i].rotation = body_segments[i-1].rotation
 		
 	var target_pos = position + (dir * grid_size)
 	position = target_pos
+	rotation = dir.angle()
 
-func _on_area_2d_body_entered(body: Node2D) -> void:
+func pick_up_object() -> void:
 	# increase current score
 	var current_score = get_parent().get_node("CanvasLayer/Score")
 	current_score.text = str(int(current_score.text) + 1)
 	
+	# add new segment to the Snake
+	add_initial_segments()
+	
 	# generate new coordiante to the object
-	var object = get_parent().get_node("Area2D")
+	var object = get_parent().get_node("ObjectArea2D")
 	object.position = generate_random_grid_position()
 	
 func generate_random_grid_position():
